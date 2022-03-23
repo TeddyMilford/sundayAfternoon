3b602b6a5c7ff7958d54233ab07688798024c171
Teddy Milford <teddy.milford@gmail.com>
2022-03-23 11:21:07 -0500

    final push

--- script.js
+++ script.js
@@ -2,19 +2,14 @@
 
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.getElementById("close");
-
-const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
+const btnCloseModal = document.getElementById("exit");
 
 window.addEventListener("load", function () {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 });
 
-btnCloseModal.addEventListener("click", closeModal());
-
-const testBtn = document.getElementById("test");
-testBtn.onclick(console.log("Hello"));
+btnCloseModal.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
