3c370bf0758a20fdb2a570b3327e59f516b1d0c0
Teddy Milford <teddy.milford@gmail.com>
2022-03-23 11:02:21 -0500

    80% there I believe

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-const gitTest = "This is just a test. Please just be this line.";
+"use strict";
 
-const prayer = "Please God - let this commit to git.";
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.getElementById("close");
 
-const number = 1000000;
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+window.addEventListener("load", function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+});
+
+btnCloseModal.addEventListener("click", closeModal());
+
+const testBtn = document.getElementById("test");
+testBtn.onclick(console.log("Hello"));
